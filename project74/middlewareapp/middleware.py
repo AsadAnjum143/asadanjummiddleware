ed694c51428a918f09bdc3044a9999e816854293
Asad <asadanjum54321@gmail.com>
2024-02-20 16:13:59 +0530

    first

--- project74/middlewareapp/middleware.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-class MiddlewareappConfig(object):
-	def __init__(self,get_response):
-		self.get_response = get_response
-
-	def __call__(self,request):
-		data = '<h1>Server is under maintainance, please re-visit our server after couple of hours</h1>'
-		return HttpResponse(data)
